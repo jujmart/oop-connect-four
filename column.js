bbd060404fa092e84fad7ce91b9cbbf9dae03d40
Justice Martin <jujmart12@gmail.com>
2021-05-05 16:17:45 -0400

    Added add method to column.js and added getTokenAt method to Game class

---
+++ column.js
@@ -0,0 +1,18 @@
+export class Column {
+    constructor(rowArr = [null, null, null, null, null, null]) {
+        this.rowArr = rowArr;
+    }
+
+    add(playerNum) {
+        for (let i = 5; i >= 0; i--) {
+            if (!this.rowArr[i]) {
+                this.rowArr[i] = playerNum;
+                return;
+            }
+        }
+    }
+
+    getTokenAt(rowIndexNum) {
+        return this.rowArr[rowIndexNum];
+    }
+}
--- game.js
+++ game.js
@@ -1,19 +1,27 @@
+import { Column } from "./column";
+
 export class Game {
     constructor(player1Name, player2Name) {
         this.name1 = player1Name;
         this.name2 = player2Name;
         this.currentPlayer = 1;
+        this.columns = [new Column(), new Column(), new Column(), new Column(), new Column(), new Column(), new Column()];
     }
 
     getName() {
         return `${this.name1} vs. ${this.name2}`;
     }
 
-    playInColumn() {
+    playInColumn(columnIndexNum) {
+        this.columns[columnIndexNum].add(this.currentPlayer);
         if (this.currentPlayer === 1) {
             this.currentPlayer = 2;
         } else {
             this.currentPlayer = 1;
         }
+    }
+
+    getTokenAt(rowIndexNum, columnIndexNum) {
+        return this.columns[columnIndexNum].getTokenAt(rowIndexNum);
     }
 }

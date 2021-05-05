6bad654ddff0a054e08568eb94b52ef18f98d017
Grant Chiu <grantchiu00@gmail.com>
2021-05-05 10:45:41 -0700

    created button

--- connect-four.js
+++ connect-four.js
@@ -1,1 +1,23 @@
 import { Game } from "./game.js";
+
+let game
+
+window.addEventListener('DOMContentLoaded', event => {
+    const playerOneInput = document.getElementById('player-1-name')
+    const playerTwoInput = document.getElementById('player-2-name')
+    const newGameBtn = document.getElementById('new-game')
+
+    function disabledNewGameBtn() {
+        if (playerOneInput.value !== '' && playerTwoInput.value !== '') {
+            newGameBtn.disabled = false;
+        } else {
+            newGameBtn.disabled = true;
+        }
+    }
+
+    playerOneInput.addEventListener('keyup', event => {
+
+
+    })
+
+})

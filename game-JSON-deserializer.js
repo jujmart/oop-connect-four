7a41f0e5f3fe051dfe7786417c7b9fe17cd0ba51
Justice Martin <jujmart12@gmail.com>
2021-05-06 18:44:21 -0400

    Fixed players turn upon refresh

--- game-JSON-deserializer.js
+++ game-JSON-deserializer.js
@@ -23,13 +23,20 @@ export class GameJsonDeserializer {
         this.column5 = JSON.parse(localStorage.getItem("column5"));
         this.column6 = JSON.parse(localStorage.getItem("column6"));
 
+        let counter = 0;
         for (let columnI = 0; columnI <= 6; columnI++) {
             let currentColumn = 'column' + columnI;
             for (let i = this[currentColumn].length - 1; i >= 0; i--) {
                 let currentPlayerNum = this[currentColumn][i];
                 this.game.currentPlayer = currentPlayerNum;
                 this.game.playInColumn(columnI);
+                counter++;
             }
+        }
+        if (counter % 2 === 0) {
+            this.game.currentPlayer = 1;
+        } else {
+            this.game.currentPlayer = 2;
         }
     }
 }
